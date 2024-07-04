a6261dd315bdd29facf695ccfb72e23900000b84
Bright Philip <chibuzorphilip2001@gmail.com>
2024-07-04 11:21:38 +0100

    Latest

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,9 +1,14 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 import { IoSend } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
 import { TbMessageCircleOff } from "react-icons/tb";
+import Picker from '@emoji-mart/react'
+import data  from '@emoji-mart/data'
+import previewPosition  from '@emoji-mart/data'
+
 
 const Chat = () => {
+
 
     const navigate = useNavigate()
     const [messages, setMessages] = useState([]);
@@ -69,8 +74,8 @@ const Chat = () => {
 
 
   
-    const sendMessage = () => {
-
+    const sendMessage = (e) => {
+        e.preventDefault();
         setIsloading(true)
         const socket = new WebSocket(wsEndpoint);
         socket.onopen = () => {
@@ -85,11 +90,38 @@ const Chat = () => {
 
         };
         setMessage('')
+        setShowPicker(false);
 
     };
 
+    const chatContainerRef = useRef(null);
+
+    useEffect(() => {
+        if (chatContainerRef.current) {
+          chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+      }, [messages, messages1]);
+    
 
     console.log(messages1.length);
+
+
+    const textareaRef = useRef(null);
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        textarea.style.height = 'auto';
+        textarea.style.height = `${textarea.scrollHeight}px`;
+    }, [message]);
+
+      
+
+      const [showPicker, setShowPicker] = useState(false);
+
+      const addEmoji = (e) => {
+        let emoji = e.native;
+        setMessage((prevText) => prevText + emoji);
+      };
+
 
   return (
     <div>
@@ -102,7 +134,7 @@ const Chat = () => {
 
 
         {messages1 && messages1.length === 0 && messages.length === 0 ? (
-            <div className='flex h-screen m justify-center items-center'>
+            <div className='flex h-screen justify-center items-center'>
                 <div className='text-center'>
                     <p className='flex m-auto justify-center text-4xl opacity-35'><TbMessageCircleOff /></p>
                     <p className='text-xs'>No Message</p>
@@ -110,16 +142,16 @@ const Chat = () => {
             </div>
         ) : (
 
-            <div className='2xl:px-[10rem] px-5 w-full py-20 h-[90vh] overflow-y-scroll'>
+            <div ref={chatContainerRef} className='2xl:px-[10rem] px-5 w-full py-20 lg:h-[90vh] h-[90vh] overflow-y-scroll'>
                 {messages1.map((msg, index) => (
                     
-                    <div className='flex flex-col gap-10 w-full'>
+                    <div className='flex flex-col gap-10 w-full' key={index}>
 
                         {sender === msg.sender ? (
 
                             <div class="chat chat-end my-2">
                                 <div className='flex items-center gap-3'>
-                                    <div class="chat-bubble bg-blue-700  text-white">
+                                    <div class="chat-bubble bg-blue-700  text-white w-fit max-w-xs break-words">
                                         <p className='text-[10px] opacity-70 text-left'>{msg.sender}</p>
                                         <p className='text-xs'>{msg.message}</p>
                                     </div>
@@ -132,7 +164,7 @@ const Chat = () => {
                             <div className="chat chat-start my-2">
                                 <div className='flex items-center gap-3'>
                                 <p className='bg-blue-800 text-white p-2 px-3 flex justify-center items-center rounded-full text-xs'>{msg.sender.slice(0,1).toLocaleUpperCase()}</p>
-                                <div className="chat-bubble bg-neutral-100  text-black">
+                                <div className="chat-bubble bg-neutral-100 text-black max-w-xs break-words">
                                     <p className='text-[10px] opacity-70 text-left'>{msg.sender}</p>
                                     <p className='text-xs'>{msg.message}</p>
                                 </div>
@@ -145,13 +177,13 @@ const Chat = () => {
 
                 {messages.map((msg, index) => (
                     
-                    <div className='flex flex-col gap-3 w-full'>
+                    <div className='flex flex-col gap-3 w-full' key={index}>
 
                     {sender === msg.message.sender ? (
 
                         <div class="chat chat-end my-2">
                             <div className='flex items-center gap-3'>
-                                <div class="chat-bubble bg-blue-700  text-white">
+                                <div class="chat-bubble bg-blue-700  text-white max-w-xs break-words">
                                     <p className='text-[10px] opacity-70 text-left'>{msg.message.sender}</p>
                                     <p className='text-xs'>{msg.message.message}</p>
                                 </div>
@@ -163,7 +195,7 @@ const Chat = () => {
                         <div className="chat chat-start my-2">
                             <div className='flex items-center gap-3'>
                             <p className='bg-blue-800 text-white p-2 px-3 flex justify-center items-center rounded-full text-xs'>{msg.message.sender.slice(0,1).toLocaleUpperCase()}</p>
-                            <div className="chat-bubble bg-neutral-100  text-black">
+                            <div className="chat-bubble bg-neutral-100  text-black max-w-xs break-words">
                                 <p className='text-[10px] opacity-70 text-left'>{msg.message.sender}</p>
                                 <p className='text-xs'>{msg.message.message}</p>
                             </div>
@@ -176,20 +208,37 @@ const Chat = () => {
         )}
 
 
-        <div className=' absolute bottom-0 py-5 pt-5 border-t bg-white border-neutral-200 w-full right-0 left-0 justify-center 2xl:px-[10rem] px-5'>
+        <div className=' fixed  bottom-0 py-5 pt-5 border-t bg-white border-neutral-200 w-full right-0 left-0 justify-center 2xl:px-[10rem] px-5'>
 
-        <div className='flex relative'>
-            <input type="text" 
+        <form className='flex relative items-center gap-3 w-full' onSubmit={sendMessage}>
+
+            <div className='relative w-full flex'>
+            <textarea type="text" 
                 placeholder="Enter Message" 
-                className="input text-xs input-bordered w-full 2xl:py-7 py-4 rounded-full" 
+                ref={textareaRef}
+                className="input text-xs input-bordered w-full resize-none lg:max-h-[6rem] min-h-[2rem] max-h-[6rem] overflow-auto py-3 lg:pr-20 pr-10 rounded-lg" 
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
             />
-            <button disabled={!message} type='button' onClick={sendMessage} className="border-none bg-blue-700 text-white absolute text-xs flex gap-3 items-center rounded-full py-3 lg:px-5 px-4 lg:right-1.5 right-0 lg:top-1 h-[3rem]">
-                {isLoading === false ?  <>Send <IoSend /></>  : <span className="loading loading-spinner loading-md"></span>}
+            <div className='absolute right-3 lg:ml-5 ml-16 lg:bottom-1 bottom-2 top-3 lg:p-3 p-3  bg-white flex justify-center items-center  rounded-full cursor-pointer' onClick={() => setShowPicker((val) => !val)}>
+            <img
+                className="emoji-icon lg:w-6 w-6"
+                src="https://icons.getbootstrap.com/assets/icons/emoji-smile.svg"
+            />
+            </div>
+            </div>
+
+            {showPicker && (
+                <span className='w-full absolute bottom-[100px] left-0'>
+                    <Picker data={data} onEmojiSelect={addEmoji} className=""/>
+                </span>
+            )}
+
+            <button disabled={!message} type='submit'  className="border-none bg-blue-700 text-white text-base flex gap-3 items-center p-3 rounded-full justify-center ">
+                {isLoading === false ?  <IoSend /> : <span className="loading loading-spinner loading-sm"></span>}
             </button>
-        </div>
-        </div>
+        </form>
+        </div> 
     </div>
   )
 }
